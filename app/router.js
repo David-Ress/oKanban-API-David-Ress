f11ebb943c9ef5e0891651fe582899b6d861a5cd
David-Ress <david.ressicaud@hotmail.fr>
2023-05-30 19:47:22 +0200

    first commit

---
+++ app/router.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const listController = require ('./controllers/listController');
+const cardController = require ('./controllers/cardController');
+const tagController = require ('./controllers/tagController');
+
+router.get('/lists', listController.getAll);
+router.get('/lists/:id', listController.getOne);
+router.delete('/lists/:id', listController.delete);
+router.post('/lists', listController.create);
+router.patch('/lists/:id', listController.update);
+
+router.get('/lists/:id/cards', cardController.getCardsInList);
+router.get('/cards', cardController.getAll);
+router.get('/cards/:id', cardController.getOne);
+router.delete('/cards/:id', cardController.delete);
+router.post('/cards', cardController.create);
+router.patch('/cards/:id', cardController.update);
+
+router.get('/tags', tagController.getAll);
+router.get('/tags/:id', tagController.getOne);
+router.delete('/tags/:id', tagController.delete);
+router.post('/tags', tagController.create);
+router.patch('/tags/:id', tagController.update);
+router.post('/cards/:id/tags', tagController.associateToCard);
+router.delete('/cards/:cardId/tags/:tagId', tagController.removeFromCard);
+
+module.exports = router;
